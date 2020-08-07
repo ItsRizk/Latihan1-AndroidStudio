0f671f677168d28d8b9c9c786e07f4a81f9fc94e
Rizky Adityana <rizkyadit.siswa@smkn4bdg.sch.id>
2020-08-07 21:26:45 +0700

    Source code

---
+++ app/src/main/java/com/example/latihan1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+}
